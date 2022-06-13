c1b596f504b621397dc7586be32a1be3422bd568
Ashita Nihore <ashita_n@me.iitr.ac.in>
2022-06-13 22:16:57 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, {Component} from "react";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Navbar from "./components/layout/Navbar";
+import Dashboard from "./components/layout/Dashboard";
+import Pokemon from './components/pokemon/Pokemon';
+import backgroundImage from './pattern.png'
+
+class App extends Component{
+  render() {
+    return (
+      <Router>
+        <div className="App" style={{background: `url(${backgroundImage})`}}>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
